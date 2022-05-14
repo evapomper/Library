7195a3bfc0872262e87c18a47ede3f573e384f24
Eugenia <eugenia.altunina@gmail.com>
2022-05-14 16:46:58 +0300

    add installed apps api and rest_framework

--- library_project/settings.py
+++ library_project/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+ 
+    # Third-party
+    'rest_framework',
 
     # Local
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig'
+
 ]
 
 MIDDLEWARE = [
